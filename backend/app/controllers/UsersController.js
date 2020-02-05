2a416aea86d8ac971978b660db667a314ceaccd1
samarone <samarone@lbc.com.br>
2020-02-05 08:34:51 -0300

    Adicionado validações e tratativas de exceção para usuários.

--- backend/app/controllers/UsersController.js
+++ backend/app/controllers/UsersController.js
@@ -7,82 +7,124 @@ module.exports = {
     async getUsers(req, res) {      // Testado: OK
         console.log('chegou em "controller>UsersController.getUsers"');
 
-        const users = await User.findAll({
-            include: {
-                model: Department
-            }
-        });
+        try {
 
-        return res.json(users);
+            const users = await User.findAll({
+                include: {
+                    model: Department
+                }
+            });
+
+            return res.json(users);
+
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
+        }
+
     },
 
     async getUser(req, res) {       // Testado: OK
         console.log('chegou em "controller>UsersController.getUser"');
 
         const { user_id } = req.params;
-        const user = await User.findByPk(user_id, {
-            include: {
-                model: Department
-            }
-        });
 
-        // let pass = user.password;
-        // pass = Crypt.decrypt(pass);
+        try {
 
-        // user['password'] = pass;
+            if (!user_id)
+                throw new Error("user index is required.");
 
-        return res.json(user);
+            const user = await User.findByPk(user_id, {
+                include: {
+                    model: Department
+                }
+            });
+
+            if (!user)
+                throw new Error("user not found.");
+
+            // let pass = user.password;
+            // pass = Crypt.decrypt(pass);
+
+            // user['password'] = pass;
+
+            return res.json(user);
+
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
+        }
+
     },
 
     async postUser(req, res) {      // Testado: OK
         console.log('chegou em "controller>UsersController.postUser"');
 
-        const { email } = req.body;
-        const user_temp = await User.findOne({ where: { email: email } });
+        try {
 
-        if (user_temp) {
-            return res.status(400).json({ error: 'user email already registered.' });
-        }
+            const { name, email, password, department } = req.body;
 
-        const { department } = req.body;
-        if (department) {
-            if (department.name != '') {
-                const [dep] = await Department.findOrCreate({
-                    where: {
-                        name: department.name,
+            if (!name)
+                throw new Error("field 'name' is required.");
+
+            if (!email)
+                throw new Error("field 'email' is required.");
+
+            if (!password)
+                throw new Error("field 'password' is required.");
+
+            const user_temp = await User.findOne({ where: { email: email } });
+
+            if (user_temp) {
+                return res.status(400).json({ error: 'user email already registered.' });
+            }
+
+            if (department) {
+                if (department.name != '') {
+                    const [dep] = await Department.findOrCreate({
+                        where: {
+                            name: department.name,
+                        }
+                    })
+
+                    if (dep.dataValues.id > 0) {
+                        req.body['department_id'] = dep.dataValues.id;
                     }
-                })
-
-                if (dep.dataValues.id > 0) {
-                    req.body['department_id'] = dep.dataValues.id;
                 }
             }
+
+            let pass = req.body.password;
+            pass = Crypt.encrypt(pass);
+
+            req.body['password'] = pass;
+
+            const user = await User.create(req.body);
+            return res.json(user);
+
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
         }
 
-        let pass = req.body.password;
-        pass = Crypt.encrypt(pass);
-
-        req.body['password'] = pass;
-
-        const user = await User.create(req.body);
-        return res.json(user);
     },
 
     async deleteUser(req, res) {    // Testado: OK
         console.log('chegou em "controller>UsersController.deleteUser"');
 
-        const { user_id } = req.params;
-        const user = await User.findByPk(user_id);
+        try {
+            const { user_id } = req.params;
+            const user = await User.findByPk(user_id);
 
-        if (!user) {
-            return res.status(400).json({ error: 'User not found TO DELETE' });
+            if (!user)
+                throw new Error("User not found TO DELETE");
+
+            await user.destroy({
+                where: {
+                    id: user_id,
+                }
+            });
+
+
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
         }
-
-        await user.destroy({
-            where: {
-                id: user_id,
-            }
-        });
 
         return res.json();
     },
@@ -90,37 +132,53 @@ module.exports = {
     async putUser(req, res) {       // Testado: OK
         console.log('chegou em "controller>UsersController.putUser"');
 
-        const { user_id } = req.params;
-        const user = await User.findByPk(user_id);
+        try {
 
-        if (!user) {
-            return res.status(400).json({ error: 'User not found TO UPDATE' });
-        }
+            const { user_id } = req.params;
+            const user = await User.findByPk(user_id);
 
-        const { department } = req.body;
-        if (department) {
-            if (department.name != '') {
-                const [dep] = await Department.findOrCreate({
-                    where: {
-                        name: department.name,
+            if (!user)
+                throw new Error("User not found TO UPDATE");
+
+            const { name, email, password, department } = req.body;
+
+            if (!name)
+                throw new Error("field 'name' is required.");
+
+            if (!email)
+                throw new Error("field 'email' is required.");
+
+            if (!password)
+                throw new Error("field 'password' is required.");
+
+            if (department) {
+                if (department.name != '') {
+                    const [dep] = await Department.findOrCreate({
+                        where: {
+                            name: department.name,
+                        }
+                    })
+
+                    if (dep.dataValues.id > 0) {
+                        req.body['department_id'] = dep.dataValues.id;
                     }
-                })
-
-                if (dep.dataValues.id > 0) {
-                    req.body['department_id'] = dep.dataValues.id;
                 }
             }
+
+            await user.update(req.body, {
+                where: {
+                    id: user_id
+                },
+                returning: true,
+                plain: true
+            });
+
+            return res.json(user);
+
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
         }
 
-        await user.update(req.body, {
-            where: {
-                id: user_id
-            },
-            returning: true,
-            plain: true
-        });
-
-        return res.json(user);
     },
 
 };
